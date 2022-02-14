8033416bf163f2c4de79d4929ba9e83a7c7d7ec1
haecheol-shin <dev.hc.shin@gmail.com>
2022-02-14 23:28:13 +0900

    2022.02.14

--- CodeUp/6096_list.py
+++ CodeUp/6096_list.py
@@ -2,7 +2,33 @@ a = []
 
 for i in range(19):
     a.append([])
+    for j in range(19):
+        a[i].append(0)
 
 for i in range(19):
-    a[i].append(0)
+    a[i] = list(map(int, input().split()))
 
+n = int(input())
+
+for i in range(n):
+    x, y = map(int, input().split())
+
+    for i in range(19):
+        for j in range(19):
+            if (i==(x-1)):
+                if (a[x-1][j]==0):
+                    a[x-1][j]=1
+                else:
+                    a[x-1][j]=0
+
+            if (j==(y-1)):
+                if (a[i][y-1]==0):
+                    a[i][y-1]=1
+                else:
+                    a[i][y-1]=0           
+
+
+for i in range(19):
+    for j in range(19):
+        print(a[i][j], end=' ')
+    print()
