bc1d5cf8d55dc1eeabb1b001141177206b5223a7
haecheol-shin <dev.hc.shin@gmail.com>
2022-02-17 22:19:20 +0900

    2022.02.17

--- CodeUp/6098_list.py
+++ CodeUp/6098_list.py
@@ -8,14 +8,26 @@ for i in range(10):
 for i in range(10):
     a[i] = list(map(int, input().split()))
 
+x, y = int(1)
+
+break2 = False
 for i in range(1,10):
     for j in range(1,10):
-        if (a[j+1][i]==1 or a[j+1][i]==2):
-            i += 1
-            break
+        if (a[x][y]==0):
+            a[x][y] = 9
+        
+        elif (a[x][y]==2):
+            a[x][y] = 9
+
+
         else:
             a[i][j] = 9
-
+            break2 = True
+            break
+    
+    if(break2==False):
+        break
+    
 for i in range(10):
     for j in range(10):
         print(a[i][j], end=" ")
