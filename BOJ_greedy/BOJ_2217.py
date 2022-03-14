f347b8b04cffc44363553f4ce8dfb8217e939c21
haecheol-shin <dev.hc.shin@gmail.com>
2022-03-14 21:04:32 +0900

    2022.03.14

--- BOJ_greedy/BOJ_2217.py
+++ BOJ_greedy/BOJ_2217.py
@@ -4,9 +4,13 @@ rope = [] # 로프가 들 수 있는 무게
 for i in range(n):
     rope.append(int(input()))
 
-min = 10000 # 가장 약한 로프
-for i in range(n):
-    if (min>rope[i]):
-        min = rope[i]
+rope.sort(reverse=True) # 로프 무게가 큰 순서
 
-print(min*n)
+rope_sum = [] #  로프로 들 수 있는 최대중량
+
+for i in range(1, n+1): # 큰 무게 X 1, 그 다음 무게 X 2 .... 가장 가벼운 무게 X n
+    rope_sum.append(i*rope[i-1])
+
+rope_sum.sort(reverse=True) # 최대중량이 큰 순서
+
+print(rope_sum[0])
