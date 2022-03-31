bb63d92689ff4c64c09357b8a28fe9552d3c8828
haecheol-shin <dev.hc.shin@gmail.com>
2022-03-31 23:22:13 +0900

    2022.03.31

--- BOJ_greedy/BOJ_1715.py
+++ BOJ_greedy/BOJ_1715.py
@@ -6,20 +6,34 @@ for i in range(n):
 
 # 3개의 뭉치 10 20 40 최솟값은 100
 # 10+20 / 30+40 --> 10이 2번 20이 두번 40이 1번
-# 5개의 뭉치 10 20 30 40 50
-# 10+20 / 30+30 / 60+40 / 100+50
-# 30+60+100+150 = (10+20)+(10+20+30)+(10+20+30+40)+(10+20+30+40+50)
-
-#가장 낮은 숫자와 두번째 낮은 숫자가 N-1번 곱해지고 순차적으로 1씩 줄어든다
+# 먼저 최대한 작은 숫자들끼리 뭉친다
+# 그 다음 숫자들끼리 뭉친다
+# 홀수가 남았을 경우 가장 큰 숫자를 남긴다
 
 sum = 0
-card_cnt.sort()
+while(True):
 
-for i in range(n):
-    if (i==0 or i==1):
-        sum += card_cnt[i] * (n-1)
+    if (len(card_cnt)%2==0): # 카드 수가 짝수 일 경우
+        for i in range(0,len(card_cnt),2):
+            addNum = card_cnt[i] + card_cnt[i+1]
+            card_cnt[i] = addNum
+            card_cnt[i+1] = addNum
+
+    else: # 카드 수가 홀수 일 경우 마지막 카드를 그대로 놔두기위해 반복문 길이를 하나 줄임
+        for i in range(0,len(card_cnt)-1,2):
+            addNum = card_cnt[i] + card_cnt[i+1]
+            card_cnt[i] = addNum
+            card_cnt[i+1] = addNum # 뭉친 숫자를 더했던 공간에 똑같은 숫자를 넣어줌
+
+    for j in range(len(card_cnt)): # 홀수 인덱스인 곳은 요소를 0으로 바꿈
+        if (j%2==1):
+            sum += card_cnt[j]
+            card_cnt[j] = 0
     
-    else:
-        sum += card_cnt[i] * (n-i)
+    card_cnt.sort() # 오름차순 정렬
+    card_cnt = card_cnt[int(len(card_cnt)/2):] # 요소가 0인 부분은 삭제
+
+    if (len(card_cnt)==1):
+        break
 
 print(sum)
