aa840ec3eec9efaed4dc3e7a11952a676ecbd185
haecheol-shin <dev.hc.shin@gmail.com>
2022-04-11 23:26:05 +0900

    2022.04.11

--- BOJ_DataStructure/BOJ_10828.py
+++ BOJ_DataStructure/BOJ_10828.py
@@ -2,31 +2,32 @@ import sys
 
 n = int(sys.stdin.readline()) # 명령 개수
 
-stack = []
-userCommandList = []
-for i in range(n):
+stack = [] # 수를 저장하는 stack
+userCommandList = [] # 유저의 명령을 저장하는 list
+
+for i in range(n): # 명령 개수 만큼 반복
     userCommand = sys.stdin.readline()
-    userCommandList = list(userCommand.split())
+    userCommandList = list(userCommand.split()) # push가 입력되는 경우 push와 수를 공백단위로 구별
     if (userCommandList[0]=='push'):
-        stack.append(userCommandList[1])
+        stack.append(userCommandList[1]) # 수를 append()로 추가
 
     elif (userCommandList[0]=='top'):
-        if (len(stack)==0):
+        if (len(stack)==0): # 스택에 아무것도 없다면 -1 출력
             print(-1)
         else:
-            print(stack[-1])
+            print(stack[-1]) # 인덱스를 -1로 설정해서 스택의 top을 출력
     
-    elif (userCommandList[0]=='size'):
-        print(len(stack))
+    elif (userCommandList[0]=='size'): 
+        print(len(stack)) # len()으로 size출력
 
     elif (userCommandList[0]=='pop'):
-        if (len(stack)==0):
+        if (len(stack)==0): # pop할게 없다면 -1 출력
             print(-1)
         else:
-            print(stack.pop())
+            print(stack.pop()) # pop()으로 pop출력
         
     elif(userCommandList[0]=='empty'):
-        if (len(stack)==0):
+        if (len(stack)==0): # len()으로 스택이 비어있는지 확인
             print(1)
         else:
             print(0)
--- BOJ_DataStructure/BOJ_1874.py
+++ BOJ_DataStructure/BOJ_1874.py
@@ -1,17 +1,14 @@
-# 조건: 들어가있는 숫자보다 출력하려는 숫자가 크면 안된다
-# 출력하려는 숫자를 출력하기 위해서 부족한 숫자만큼 push
-# pop을 하면 top에 있는 숫자가 하나빠짐
-# push횟수: 4 pop횟수: 0 출력가능한 최대숫자: 4
-# --> if(출력숫자<=push횟수-pop횟수) 이면 출력 가능
-# else: 출력불가능 print("NO")
-
 import sys
 
 n = int(sys.stdin.readline())
+arr = [i for i in range(1, n+1)] # 1부터 시작되는 오름차순 리스트 생성
+inputSequence = [] # 수열을 입력받을 리스트 
+result = [] # 결과 값을 저장하는 리스트
 
-arr = []
 for i in range(n):
-    arr.append(int(sys.stdin.readline()))
+    inputSequence.append(int(sys.stdin.readline()))
 
+index = -1
 for i in range(n):
-    for j in range()
+    index = inputSequence[i] - (i+1)
+
