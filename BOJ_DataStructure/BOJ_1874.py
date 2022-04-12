25f212f41cf5650b485e3cb731e179e351011f12
haecheol-shin <dev.hc.shin@gmail.com>
2022-04-12 23:44:30 +0900

    2022.04.12

--- BOJ_DataStructure/BOJ_1874.py
+++ BOJ_DataStructure/BOJ_1874.py
@@ -1,14 +1,33 @@
 import sys
 
 n = int(sys.stdin.readline())
-arr = [i for i in range(1, n+1)] # 1부터 시작되는 오름차순 리스트 생성
 inputSequence = [] # 수열을 입력받을 리스트 
 result = [] # 결과 값을 저장하는 리스트
 
 for i in range(n):
     inputSequence.append(int(sys.stdin.readline()))
 
-index = -1
-for i in range(n):
-    index = inputSequence[i] - (i+1)
+pushCount = 0 # 수열이 반복문을 돌아가면서 가장 높은 수를 저장
+plusCount = 0 # +를 몇번 찍어야하는지 저장하는 변수
 
+for i in range(n):
+    if (i==0):
+        pass
+
+    elif (inputSequence[i-1]-inputSequence[i]<=1):
+        pass
+    
+    else:
+        print("NO")
+    
+    if (inputSequence[i]>pushCount):
+        pushCount = inputSequence[i] # 가장 높은 수를 저장
+        plusCount = pushCount - plusCount # 가장 높은 수 - 이미 저장된 +개수
+        for j in range(plusCount):
+            result.append('+')
+    
+    
+    result.append('-')
+
+for i in range(n):
+    print(result[i])
