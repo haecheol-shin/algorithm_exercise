5b3bdd24257b2e84e48652f04193e8133a8bc056
haecheol-shin <dev.hc.shin@gmail.com>
2022-05-06 23:22:57 +0900

    2022.05.06

---
+++ BOJ_DataStructure/BOJ_17298.py
@@ -0,0 +1,20 @@
+from msilib import sequence
+import sys
+
+n = int(sys.stdin.readline()) # 수열의 크기
+intSequence = list(map(int, input().split()))
+result = []
+
+for i in range(n):
+    for j in range(i+1, n):
+        if (intSequence[i]<intSequence[j]):
+            result.append(intSequence[j])
+            break
+
+        if (j==n-1):
+            result.append(-1)
+            break
+
+result.append(-1)
+
+print(result)
