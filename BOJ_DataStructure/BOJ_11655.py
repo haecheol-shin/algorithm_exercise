13c02830736dea45b7daaede4493d6d298118f04
haecheol-shin <dev.hc.shin@gmail.com>
2022-05-20 18:40:05 +0900

    2022.05.20

---
+++ BOJ_DataStructure/BOJ_11655.py
@@ -0,0 +1,36 @@
+upperLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowerLetter = "abcdefghijklmnopqrstuvwxyz"
+
+upperDic = {}
+lowerDic = {}
+
+i = 0
+for spelling in upperLetter:
+    upperDic[spelling] = i
+    i = i+1
+
+j = 0
+for spelling in lowerLetter:
+    lowerDic[spelling] = j
+    j = j+1
+
+sentence = input()
+result = ''
+
+for letter in sentence:
+    if letter.isupper():
+        upperIndex = upperDic[letter] + 13
+        if upperIndex > 25:
+            upperIndex = upperIndex - 25
+        result = result + str(upperDic.get(upperIndex))
+
+    elif letter.islower():
+        lowerIndex = lowerDic[letter] + 13
+        if lowerIndex > 25:
+            lowerIndex = lowerIndex - 25
+        result = result + str(lowerDic.get(lowerIndex))
+    
+    else:
+        result = result + letter
+
+print(result)
