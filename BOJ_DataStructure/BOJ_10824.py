649939fcd383af4b29e33082cfcb72aee529826b
haecheol-shin <dev.hc.shin@gmail.com>
2022-05-23 10:15:55 +0900

    2022.05.23

---
+++ BOJ_DataStructure/BOJ_10824.py
@@ -0,0 +1,6 @@
+n = list(map(int, input().split()))
+
+aPlusb = str(n[0])+str(n[1])
+cPlusd = str(n[2])+str(n[3])
+
+print(int(aPlusb)+int(cPlusd))
--- BOJ_DataStructure/BOJ_11655.py
+++ BOJ_DataStructure/BOJ_11655.py
@@ -1,34 +1,55 @@
-upperLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-lowerLetter = "abcdefghijklmnopqrstuvwxyz"
+# upperLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+# lowerLetter = "abcdefghijklmnopqrstuvwxyz"
 
-upperDic = {}
-lowerDic = {}
+# upperDic = {}
+# lowerDic = {}
 
-i = 0
-for spelling in upperLetter:
-    upperDic[spelling] = i
-    i = i+1
+# i = 0
+# for spelling in upperLetter:
+#     upperDic[spelling] = i
+#     i = i+1
 
-j = 0
-for spelling in lowerLetter:
-    lowerDic[spelling] = j
-    j = j+1
+# j = 0
+# for spelling in lowerLetter:
+#     lowerDic[spelling] = j
+#     j = j+1
+
+# sentence = input()
+# result = ''
+
+# for letter in sentence:
+#     if letter.isupper():
+#         upperIndex = upperDic[letter] + 13
+#         if upperIndex > 25:
+#             upperIndex = upperIndex - 25
+#         result = result + str(upperDic.get(upperIndex))
+
+#     elif letter.islower():
+#         lowerIndex = lowerDic[letter] + 13
+#         if lowerIndex > 25:
+#             lowerIndex = lowerIndex - 25
+#         result = result + str(lowerDic.get(lowerIndex))
+    
+#     else:
+#         result = result + letter
+
+# print(result)
 
 sentence = input()
 result = ''
 
 for letter in sentence:
     if letter.isupper():
-        upperIndex = upperDic[letter] + 13
-        if upperIndex > 25:
-            upperIndex = upperIndex - 25
-        result = result + str(upperDic.get(upperIndex))
+        rot13 = ord(letter)+13
+        if rot13 > 90:
+            rot13 = rot13 - 26
+        result = result + chr(rot13)
 
     elif letter.islower():
-        lowerIndex = lowerDic[letter] + 13
-        if lowerIndex > 25:
-            lowerIndex = lowerIndex - 25
-        result = result + str(lowerDic.get(lowerIndex))
+        rot13 = ord(letter)+13
+        if rot13 > 122:
+            rot13 = rot13 - 26
+        result = result + chr(rot13)
     
     else:
         result = result + letter
