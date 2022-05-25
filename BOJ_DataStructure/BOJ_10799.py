7c6ed1669b8b3c3208dfefab6c21a4e90c7fb427
haecheol-shin <dev.hc.shin@gmail.com>
2022-05-25 23:42:55 +0900

    2022.05.25

--- BOJ_DataStructure/BOJ_10799.py
+++ BOJ_DataStructure/BOJ_10799.py
@@ -1,38 +1,33 @@
-# <>안에 있는 단어는 큐로 출력
-# 그냥 단어는 스택으로 출력
-# 단어는 공백으로 구분하되, 단어안에 있는 공백은 단어취급
+# 쇠막대기는 자신보다 긴 쇠막대기 위에만 놓일 수 있다. - 쇠막대기를 다른 쇠막대기 위에 놓는 경우 완전히 포함되도록 놓되, 끝점은 겹치지 않도록 놓는다.
+# 각 쇠막대기를 자르는 레이저는 적어도 하나 존재한다.
+# 레이저는 어떤 쇠막대기의 양 끝점과도 겹치지 않는다. 
 
-import sys
-from collections import deque
+# 스택에 있는 ( 개수를 센다
+# () 가 나오면 ( 개수만큼 막대가 나온다.
+# ) 가 나오면 막대가 하나 나오고 ( 개수가 하나 줄어든다.
 
-sentence = sys.stdin.readline()
+stack = []
 
-result = '' # 결과를 저장
-queue = deque([]) # 단어를 저장할 데크
-temp = [] # 단어를 임시로 저장함
-signal = False
+sentence = input()
+result = 0 # 총 결과
+flag = True
+for letter in sentence:
+    
+    if letter=='(':
+        stack.append(letter)
+        flag = True
 
-for i in sentence:
-    if (signal == True):
-
-        if (i=='>'):
-            queue.append(i)
-            for i in range(len(queue)):
-                result.join(queue.popleft())
-            result.join(' ')
-            signal = False
-
-        else:
-            queue.append(i)
+    elif letter==')':
+        if stack[-1] == '(' and flag==True:
+            stack.pop()
+            result = result + len(stack)
+            flag = False
+        
+        elif flag==False:
+            stack.pop()
+            result = result + 1
+    
     else:
-        if (i=='<'):
-            queue.append(i)
-            signal = True
-
-        elif (i==' '):
-            result.join(queue.pop())
-            result.join(' ')
-        else:
-            queue.append(i)    
+        break
 
 print(result)
