bb5f2482314bfb412bb01feabeb98c19b6e00652
haecheol-shin <dev.hc.shin@gmail.com>
2022-06-20 22:20:54 +0900

    2022.06.20

--- BOJ_DataStructure/BOJ_2493.py
+++ BOJ_DataStructure/BOJ_2493.py
@@ -1,27 +1,45 @@
+# 출력초과 뜸
 
-n = int(input()) # 탑의 개수
-towerList = input().split()
-print(towerList)
+def tower():
+    n = int(input()) # 탑의 개수
+    towerList = input().split()
 
-towerList.reverse()
-stack = []
-result = []
-for i in range(len(towerList)):
-    if len(stack)==0:
-        stack.append(int(towerList[i]))
-    
-    elif int(stack[-1])<int(towerList[i]):
-        result.append(n-i)
-        for tower in stack:
-            stack.pop()
-            result.append(n-i)
+    towerList.reverse()
+    stack = []
+    result = []
+    for i in range(len(towerList)):
+        if len(stack)==0:
+            stack.append(int(towerList[i]))
+        
+        elif int(stack[-1])<int(towerList[i]):
+            for _ in range(len(stack)):
+                stack.pop()
+                result.append(n-i)
+            stack.append(int(towerList[i]))
 
+        else:
+            stack.append(int(towerList[i]))
+
+    for _ in stack:
+        result.append(0)
+
+    result.reverse()
+
+    flag = 0
+    for i in range(len(result)):
+        flag = flag + result[i]
+        if i==len(result)-1:
+            if flag==0:
+                result.clear()
+                return result
+            else:
+                return result
+
+if __name__=="__main__":
+    result = tower()
+    if len(result)==0:
+        print(0)
     else:
-        stack.append(int(towerList[i]))
+        for i in result:
+            print(i, end=' ')
 
-    if i==len(towerList)-1:
-        for _ in stack:
-            result.append(0)
-
-# 역순으로 출력 바꾸고, result 값이 하나씩 밀리는 느낌
-print(result)
