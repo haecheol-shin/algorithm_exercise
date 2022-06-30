066c699bcb8383b84e60aa8fd133d907bc00196c
haecheol-shin <dev.hc.shin@gmail.com>
2022-06-30 16:46:32 +0900

    2022.06.30

--- BOJ_DataStructure/BOJ_1966.py
+++ BOJ_DataStructure/BOJ_1966.py
@@ -1,4 +1,3 @@
-from calendar import c
 import sys
 from collections import deque
 
@@ -13,19 +12,21 @@ for i in range(n):
     for j in priority:
         deque.append(j)
 
+    priority[testCase[1]] = str(priority[testCase[1]])
     count = 0
     while(True):
-        if max(deque)==deque[0]:
+        if max(deque)!=int(deque[0]):
+            deque.append(deque.popleft)
+        
+        elif max(deque)==int(deque[0]):
             count = count + 1
             deque.popleft()
 
-        else:
-            deque.append(deque.popleft())
-
-        if testCase[1]==count:
+        elif max(deque)==int(deque[0]) and type(deque[0]) is str:
+            count = count + 1
             break
-
-    # 우선순위가 같은 경우가 해결되지 않음.
     
-print(count)
+    print(count)
     
+    # 찾고자 하는 값의 위치를 저장할 변수 1개
+    # 찾고자 하는 값이 중복으로 여러 개 있는 경우 -> 그 값만 형변환해서 type검사를 해서 찾아낸다
