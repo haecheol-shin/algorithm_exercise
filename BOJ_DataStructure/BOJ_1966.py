70f365e926b75a8b61d799c8d1946644156aa282
haecheol-shin <dev.hc.shin@gmail.com>
2022-07-01 23:26:46 +0900

    2022.07.01

--- BOJ_DataStructure/BOJ_1966.py
+++ BOJ_DataStructure/BOJ_1966.py
@@ -4,29 +4,30 @@ from collections import deque
 n = int(sys.stdin.readline())
 
 deque = deque()
+
 for i in range(n):
     testCase = list(map(int, sys.stdin.readline().split()))
 
     priority = list(map(int, sys.stdin.readline().split()))
 
+    index = [ i for i in range(len(testCase[0]))]
+
     for j in priority:
         deque.append(j)
 
-    priority[testCase[1]] = str(priority[testCase[1]])
     count = 0
     while(True):
-        if max(deque)!=int(deque[0]):
-            deque.append(deque.popleft)
-        
-        elif max(deque)==int(deque[0]):
-            count = count + 1
-            deque.popleft()
-
-        elif max(deque)==int(deque[0]) and type(deque[0]) is str:
-            count = count + 1
-            break
-    
-    print(count)
+        while deque:
+            if priority[0] == max(priority):    # 나머지 문서들보다 중요도가 더 높은 문서가 없다면
+                count += 1
+                if index[0] == 'target':
+                    print(count)
+                    break
+                deque.pop(0)
+                index.pop(0)
+            else:
+                deque.append(priority.pop(0))
+                index.append(index.pop(0))
     
     # 찾고자 하는 값의 위치를 저장할 변수 1개
     # 찾고자 하는 값이 중복으로 여러 개 있는 경우 -> 그 값만 형변환해서 type검사를 해서 찾아낸다
