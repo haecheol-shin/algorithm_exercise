b4f70d314fb51560ce93609faf5285d005605230
haecheol-shin <dev.hc.shin@gmail.com>
2022-07-11 22:41:37 +0900

    2022.07.11

--- Python_DataStructure/LinkedList.py
+++ Python_DataStructure/LinkedList.py
@@ -48,30 +48,35 @@ class LinkedList:
         self.nodeCount += 1
         return True
 
-
-    def popAt(self, pos): # 리스트 안에 유일한 노드를 삭제하려 할때를 해결해야 할 듯
-        if pos < 1 or pos > self.nodeCount:
+    def popAt(self, pos):
+        if pos < 1 or pos > self.nodeCount: # 삭제하려는 원소의 위치가 인덱스 범위를 벗어난 경우
             raise IndexError("IndexError")
-            
-        if pos == 1:
-            curr = self.head
-            self.head = self.head.next
-            self.head.next = None
         
-        elif pos == self.nodeCount:
+        if self.head == self.tail: # 삭제하려는 원소가 리스트 내에서 유일한 원소인 경우
+            curr = self.head
+            self.head = None # head 조정
+            self.tail = None # tail 조정
+        # 따로 처리해야 하는 이유는 head와 tail 조정이 둘다 이뤄져야 하기 때문
+        
+        elif pos == 1: # 삭제하려는 원소의 위치가 맨 앞인 경으
+            curr = self.head
+            self.head = self.head.next # head 조정
+            curr.next = None
+        
+        elif pos == self.nodeCount: # 삭제하려는 원소의 위치가 맨 뒤인 경우
             prev = self.getAt(pos-1)
             curr = prev.next
-            self.tail = prev
+            self.tail = prev # tail 조정
             prev.next = None
             
-        else:
+        else: # 삭제하려는 원소의 위치가 중간인 경우
             prev = self.getAt(pos-1)
             curr = prev.next
             prev.next = curr.next
             curr.next = None
         
         self.nodeCount -= 1
-        return curr
+        return curr.data
 
 
     def traverse(self):
@@ -85,3 +90,5 @@ class LinkedList:
 
 def solution(x):
     return 0
+
+
