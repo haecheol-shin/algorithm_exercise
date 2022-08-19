aaf9dbca6db116b861f7b57738e37eb2d267cd7c
haecheol-shin <dev.hc.shin@gmail.com>
2022-08-19 22:25:15 +0900

    2022.08.19

--- Python_DataStructure/BinarySearchTree.py
+++ Python_DataStructure/BinarySearchTree.py
@@ -104,18 +104,22 @@ class BinSearchTree:
                 # 하나 있는 자식이 왼쪽인지 오른쪽인지를 판단하여
                 # 그 자식을 어떤 변수가 가리키도록 합니다.
                 if node.left:
-                    pass
+                    tmpNode = node.left
                 else:
-                    pass
+                    tmpNode = node.right
                 # 만약 parent 가 있으면
                 # node 가 왼쪽 자식인지 오른쪽 자식인지 판단하여
                 # 위에서 가리킨 자식을 대신 node 의 자리에 넣습니다.
                 if parent:
-                    pass
+                    if node == parent.left:
+                        parent.left = tmpNode
+
+                    else:
+                        parent.right = tmpNode
                 # 만약 parent 가 없으면 (node 는 root 인 경우)
                 # self.root 에 위에서 가리킨 자식을 대신 넣습니다.
                 else:
-                    pass
+                    self.root = tmpNode
             # When the node has both left and right children
             else:
                 parent = node
@@ -126,14 +130,19 @@ class BinSearchTree:
                 # 순환문이 종료할 때 successor 는 바로 다음 키를 가진 노드를,
                 # 그리고 parent 는 그 노드의 부모 노드를 가리키도록 찾아냅니다.
                 while successor.left:
-                    pass
+                    parent = successor
+                    successor = successor.left
                 # 삭제하려는 노드인 node 에 successor 의 key 와 data 를 대입합니다.
                 node.key = successor.key
                 node.data = successor.data
                 # 이제, successor 가 parent 의 왼쪽 자식인지 오른쪽 자식인지를 판단하여
                 # 그에 따라 parent.left 또는 parent.right 를
                 # successor 가 가지고 있던 (없을 수도 있지만) 자식을 가리키도록 합니다.
-                pass
+                if parent.left == successor:
+                    parent.left = successor.right
+
+                else:
+                    parent.right = successor.right
 
             return True
 
