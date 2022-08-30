c4fcad391a9e7f08333429708dfaa3d3709f9aa0
haecheol-shin <dev.hc.shin@gmail.com>
2022-08-30 23:29:34 +0900

    2022.08.30

--- BOJ_DataStructure/BOJ_1655.py
+++ BOJ_DataStructure/BOJ_1655.py
@@ -3,24 +3,39 @@ import heapq
 
 n = int(sys.stdin.readline())
 
-minHeap = [] # 최소값
-midHeap = [] # 중간값
-maxHeap = [] # 최댓값
+minHeap = [] # 최소 힙
+maxHeap = [] # 최대 힙
 
 for i in range(n):
     num = int(sys.stdin.readline())
-    
-    if len(minHeap) == 0:
+
+    if len(minHeap)==0 and len(maxHeap)==0:
+        middle = num
+
+    if len(minHeap) !=0 and len(minHeap) == len(maxHeap):
+        middle = (minHeap[0] + maxHeap[0][1]) / 2
+
+    if len(minHeap) - len(maxHeap) == 1:
+        middle = minHeap[0]
+
+    if len(minHeap) - len(maxHeap) == -1:
+        middle = maxHeap[0][1]
+
+    if middle >= num:
         heapq.heappush(minHeap, num)
 
-    elif len(midHeap) == 0:
-        heapq.heappush(midHeap, num)
+    else:
+        heapq.heappush(maxHeap, (-num, num))
 
-    elif len(maxHeap) == 0:
-        heapq.heappush(maxHeap, num)
+    if len(minHeap)-len(maxHeap)==2:
+        x = heapq.heappop(minHeap)
+        heapq.heappush(maxHeap, (-x, x))
 
-    if minHeap[0]
-
-    # 중간값을 가진 최소힙에서 최솟값을 반환하는 것으로 답을 한다.
-    # 숫자를 넣을 때 어떤 힙에 들어가야 될지 처리하는 과정이 필요
-    # 최소 힙과 최대 힙을 섞으면 가능할수도
+    if len(minHeap)-len(maxHeap)==-2:
+        x = heapq.heappop(maxHeap)[1]
+        heapq.heappush(minHeap, x)
+    
+    if len(maxHeap)==0:
+        print(minHeap[0])
+    else:
+        print(maxHeap[0][1])
