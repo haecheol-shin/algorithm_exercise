29c07ac58112a62188e83a7110d6ed32b19682c7
haecheol-shin <dev.hc.shin@gmail.com>
2022-09-30 23:35:30 +0900

    2022.09.30

--- BOJ_DataStructure/BOJ_2805.py
+++ BOJ_DataStructure/BOJ_2805.py
@@ -1,29 +1,24 @@
 import sys
+   
+n, m = map(int, sys.stdin.readline().split())
+height = list(map(int, sys.stdin.readline().split()))
 
+start = 1
+end = max(height)
+while(start <= end):
+    mid = (start + end) // 2
 
-def search(start, end):
-    h = end - start # 나무 톱 높이
-    sum = 0 # 자른 나무 길이의 합
-    for i in range(n):
-        if height[i]-h > 0:
-            sum += height[i] - h
-    
-    if sum > m:
-        h = h + ((h+end) // 2)
-        search(h, end)
-    
-    elif sum < m:
-        h = h//2
-        search(h, end)
+    result = 0
+    for i in height:
+        if i > mid:
+            result += i - mid
+        if result >= m:
+            break
 
-    else:
-        print(h)
-        return
-        
+    if result == m:
+        break
 
-if __name__ == "__main__":
-    n, m = map(int, sys.stdin.readline().rstrip().split())
-    height = list(map(int, sys.stdin.readline().rstrip().split()))
+    elif result > m:
+        start = mid + 1
 
-    height.sort()
-    search(0, height[-1])
+print(mid)      
